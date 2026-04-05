87d030a8c8785247ccc4e7032cef63ac8582e71d
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-04-05 04:58:01 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        name="index.html",
+        context={"request": request}
+    )
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
